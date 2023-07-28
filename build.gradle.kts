71d802a8ca950b0eaf27f05078920d635310c21d
Thomas Minke <thomas.minke@ritense.com>
2023-07-28 16:58:00 +0200

    Removed Docker Compose setup. The nl-portal-docker-compose repository should be used. Also upgraded to 0.1.0.RELEASE.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,6 @@ plugins {
     //IntelliJ IDEA
     id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.1"
     id("org.jetbrains.kotlin.plugin.spring") version "1.6.10"
-
-    //Docker
-    id("com.avast.gradle.docker-compose") version "0.16.11"
 }
 
 apply("gradle/test.gradle.kts")
@@ -41,20 +38,20 @@ repositories {
 
 dependencies {
     // Valtimo
-    implementation("nl.nl-portal:case:1.0.8.RELEASE")
-    implementation("nl.nl-portal:form:1.0.8.RELEASE")
-    implementation("nl.nl-portal:graphql:1.0.8.RELEASE")
-    implementation("nl.nl-portal:zaak:1.0.8.RELEASE")
-    implementation("nl.nl-portal:core:1.0.8.RELEASE")
-    implementation("nl.nl-portal:data:1.0.8.RELEASE")
-    implementation("nl.nl-portal:form-flow:1.0.8.RELEASE")
-    implementation("nl.nl-portal:haalcentraal:1.0.8.RELEASE")
-    implementation("nl.nl-portal:klant:1.0.8.RELEASE")
-    implementation("nl.nl-portal:messaging:1.0.8.RELEASE")
-    implementation("nl.nl-portal:product:1.0.8.RELEASE")
-    implementation("nl.nl-portal:task:1.0.8.RELEASE")
-    implementation("nl.nl-portal:common-ground-authentication:1.0.8.RELEASE")
-    implementation("nl.nl-portal:common-ground-authentication-test:1.0.8.RELEASE")
+    implementation("nl.nl-portal:case:0.1.0.RELEASE")
+    implementation("nl.nl-portal:form:0.1.0.RELEASE")
+    implementation("nl.nl-portal:graphql:0.1.0.RELEASE")
+    implementation("nl.nl-portal:zaak:0.1.0.RELEASE")
+    implementation("nl.nl-portal:core:0.1.0.RELEASE")
+    implementation("nl.nl-portal:data:0.1.0.RELEASE")
+    implementation("nl.nl-portal:form-flow:0.1.0.RELEASE")
+    implementation("nl.nl-portal:haalcentraal:0.1.0.RELEASE")
+    implementation("nl.nl-portal:klant:0.1.0.RELEASE")
+    implementation("nl.nl-portal:messaging:0.1.0.RELEASE")
+    implementation("nl.nl-portal:product:0.1.0.RELEASE")
+    implementation("nl.nl-portal:task:0.1.0.RELEASE")
+    implementation("nl.nl-portal:common-ground-authentication:0.1.0.RELEASE")
+    implementation("nl.nl-portal:common-ground-authentication-test:0.1.0.RELEASE")
 
     // Spring
     implementation("org.springframework.boot:spring-boot-starter")
@@ -71,22 +68,4 @@ tasks.test {
     useJUnitPlatform()
 }
 
-dockerCompose {
-    setProjectName(name) // uses projectRoot.name as the container group name
-
-    stopContainers.set(true) // doesn't call `docker-compose down` if set to false; default is true
-    removeContainers.set(false) // containers are retained upon composeDown for persistent storage
-}
-
-tasks.bootRun {
-    dependsOn(tasks.composeUp) // bootrun starts a composeUp subtask
-}
-
 apply(from = "gradle/deployment.gradle")
-
- gradle.taskGraph.afterTask {
-    if (this.name == "bootRun" && state.failure != null) { // gracefully stop the containers should bootRun task fail
-        logger.log(LogLevel.WARN, "Stopping docker containers gracefully.")
-        tasks.composeDown.get().down()
-    }
- }
