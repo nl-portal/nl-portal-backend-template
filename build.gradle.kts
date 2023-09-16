58ad6d1433de72f3e3d2fff9c50a28e3956bd942
Frank van der Linden <flinden68@elstarit.nl>
2023-09-16 07:26:54 +0200

    version nl-portal 0.3.0-RELEASE

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,26 +37,26 @@ repositories {
 }
 
 dependencies {
-    implementation("nl.nl-portal:case:0.2.2.RELEASE")
-    implementation("nl.nl-portal:form:0.2.2.RELEASE")
-    implementation("nl.nl-portal:graphql:0.2.2.RELEASE")
+    implementation("nl.nl-portal:case:0.3.0.RELEASE")
+    implementation("nl.nl-portal:form:0.3.0.RELEASE")
+    implementation("nl.nl-portal:graphql:0.3.0.RELEASE")
 
     //zgw
-    implementation("nl.nl-portal:zaken-api:0.2.2.RELEASE")
-    implementation("nl.nl-portal:documenten-api:0.2.2.RELEASE")
-    implementation("nl.nl-portal:catalogi-api:0.2.2.RELEASE")
-    implementation("nl.nl-portal:objectenapi:0.2.2.RELEASE")
-    implementation("nl.nl-portal:taak:0.2.2.RELEASE")
+    implementation("nl.nl-portal:zaken-api:0.3.0.RELEASE")
+    implementation("nl.nl-portal:documenten-api:0.3.0.RELEASE")
+    implementation("nl.nl-portal:catalogi-api:0.3.0.RELEASE")
+    implementation("nl.nl-portal:objectenapi:0.3.0.RELEASE")
+    implementation("nl.nl-portal:taak:0.3.0.RELEASE")
 
-    implementation("nl.nl-portal:core:0.2.2.RELEASE")
-    implementation("nl.nl-portal:data:0.2.2.RELEASE")
-    implementation("nl.nl-portal:form-flow:0.2.2.RELEASE")
-    implementation("nl.nl-portal:haalcentraal-brp:0.2.2.RELEASE")
-    implementation("nl.nl-portal:klant:0.2.2.RELEASE")
-    implementation("nl.nl-portal:messaging:0.2.2.RELEASE")
-    implementation("nl.nl-portal:product:0.2.2.RELEASE")
-    implementation("nl.nl-portal:common-ground-authentication:0.2.2.RELEASE")
-    implementation("nl.nl-portal:common-ground-authentication-test:0.2.2.RELEASE")
+    implementation("nl.nl-portal:core:0.3.0.RELEASE")
+    implementation("nl.nl-portal:data:0.3.0.RELEASE")
+    implementation("nl.nl-portal:form-flow:0.3.0.RELEASE")
+    implementation("nl.nl-portal:haalcentraal-brp:0.3.0.RELEASE")
+    implementation("nl.nl-portal:klant:0.3.0.RELEASE")
+    implementation("nl.nl-portal:messaging:0.3.0.RELEASE")
+    implementation("nl.nl-portal:product:0.3.0.RELEASE")
+    implementation("nl.nl-portal:common-ground-authentication:0.3.0.RELEASE")
+    implementation("nl.nl-portal:common-ground-authentication-test:0.3.0.RELEASE")
 
     // Spring
     implementation("org.springframework.boot:spring-boot-starter")
