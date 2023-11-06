f52db7dd0af04a2725965588fa8fa500fcf64e11
Jan Bre <johaness1@hotmail.com>
2023-11-06 09:34:28 +0100

    Also adding misssing hr dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,8 +70,9 @@ dependencies {
     implementation("nl.nl-portal:core:$backend_libraries_version")
     implementation("nl.nl-portal:data:$backend_libraries_version")
     implementation("nl.nl-portal:form-flow:$backend_libraries_version")
-    implementation("nl.nl-portal:haalcentraal-brp:$backend_libraries_version")
     implementation("nl.nl-portal:haalcentraal-all:$backend_libraries_version")
+    implementation("nl.nl-portal:haalcentraal-brp:$backend_libraries_version")
+    implementation("nl.nl-portal:haalcentraal-hr:$backend_libraries_version")
     implementation("nl.nl-portal:klant:$backend_libraries_version")
     if (!backend_libraries_version!!.equals("0.3.0.RELEASE")) {
         implementation("nl.nl-portal:klant-generiek:$backend_libraries_version")
